ca9481b83ffdcb37efcc0bfb72c0a64d36a2667b
GBagretsov <gbagretsov@gmail.com>
2016-10-23 15:53:59 +0300

    Added form that sends data to server; added POST handler stub

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
     console.log('listening on 3000');
 });
 
 app.get('/', function(req, res) {
-    res.send('get');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/api/todos', function(req, res) {
+    console.log(req.body);
+    res.redirect('/');
 });
